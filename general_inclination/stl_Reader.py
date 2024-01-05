20b85f1dca8c48c32d98be0ef659c39d561ae079
rahulaxiom <rahulranjann45@gmail.com>
2024-01-04 20:49:43 -0500

    second commit

---
+++ general_inclination/stl_Reader.py
@@ -0,0 +1,28 @@
+import vtk
+
+def main():
+
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName("inclination angle _i_ projection plane.stl")
+    
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(reader.GetOutputPort())
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    renderer = vtk.vtkRenderer()
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(0,0,0) # RGB values
+
+    renderWindow.Render()
+    renderWindowInteractor.Start()
+
+
+if __name__ == "__main__":
+    main()
